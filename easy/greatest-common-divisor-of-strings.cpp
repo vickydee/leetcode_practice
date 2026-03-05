b2902fc844d7bf16d430fbdbd9af5ae0a7e9a836
vickydee <79496219+vickydee@users.noreply.github.com>
2026-03-05 15:57:24 -0700

    Create greatest-common-divisor-of-strings.cpp

---
+++ easy/greatest-common-divisor-of-strings.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+      // str1 = ABC, str2 = ABCABC, then str1+str2 = str2+str1 (ABCABCABC)
+        if (str1+str2 == str2+str1) {
+          
+        }
+      return ""
+    }
+};
